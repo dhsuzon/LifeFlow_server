fd09eed7f76a1ca4845787b5f93ed3b5a603bd07
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2026-06-21 17:40:00 +0600

    add blood group and district upazila data

---
+++ src/data/bloodGroups.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bloodGroups: [
+    { id: "A+", name: "A+" },
+    { id: "A-", name: "A-" },
+    { id: "B+", name: "B+" },
+    { id: "B-", name: "B-" },
+    { id: "AB+", name: "AB+" },
+    { id: "AB-", name: "AB-" },
+    { id: "O+", name: "O+" },
+    { id: "O-", name: "O-" },
+  ],
+};
